1ab694c71567ce99b01cd9c4d47b71e82e151a6b
imanursar <imanursar@gitlab.com>
2019-06-21 10:55:41 +0700

    button + number

--- button/push_button_2.py
+++ button/push_button_2.py
@@ -2,15 +2,16 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 def button_callback(channel):
     print("Button was pushed!")
+    global n
     n = n + 1
-	print("Button press= ",n)
+    print("Button press= ",n)
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 n=0
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback(n)) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
